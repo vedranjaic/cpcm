dfd80c70db62526b72d074f9e838bc0997bec725
Dijana Savin <dijana.savin@neos.hr>
2019-07-19 14:21:56 +0200

    potvrdeno sa sastanka 19.07.2019.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -44,8 +44,7 @@ $(document).ready(function () {
 
 
 // Tabs
-$('#navTabs a').click(function (e) {
-    e.preventDefault();
+$('#navTabs a').click(function () {
     $('#navTabs li').removeClass('active');
     $(this).parent().addClass('active');
 })
@@ -97,6 +96,63 @@ $('#demo-page-search-results tr td:not(:last-child)').click(function () {
     $('#demo-page-search-results').toggleClass('page-sidebar--true');
     // $('.page-sidebar--profile').toggleClass('hidden');
 })
+$('#demo-page-cr-operaters tr td:not(:last-child)').click(function () {
+    $('#demo-page-cr-operaters').toggleClass('page-sidebar--true');
+    // $('.page-sidebar--profile').toggleClass('hidden');
+})
+
+$(function () { //run when the DOM is ready
+    $(".tab-user--signup").click(function () { //use a class, since your ID gets mangled     
+        $(".panel-login").removeClass("panel-header--login"); //add the class to the clicked element
+        $(".panel-login").addClass("panel-header--signin"); //add the class to the clicked element
+    });
+});
+
+$(".panel-login").on(
+    "webkitAnimationEnd oanimationend msAnimationEnd animationend",
+    function () {
+        $(this).removeClass("panel-header--signin");
+    }
+);
+$(function () { //run when the DOM is ready
+    $(".tab-user--login").click(function () { //use a class, since your ID gets mangled
+        $(".panel-login").removeClass("panel-header--signin"); //add the class to the clicked element
+        $(".panel-login").addClass("panel-header--login"); //add the class to the clicked element
+    });
+});
+
+$(".panel-login").on(
+    "webkitAnimationEnd oanimationend msAnimationEnd animationend",
+    function () {
+        $(this).removeClass("panel-header--login");
+    }
+);
+
+$(function () { //run when the DOM is ready
+    $(".tab-user--signup").click(function () { //use a class, since your ID gets mangled
+        $(".tab-user--login a").removeClass("active"); //add the class to the clicked element
+        $(".tab-user--signup a").addClass("active"); //add the class to the clicked element
+    });
+});
+$(function () { //run when the DOM is ready
+    $(".tab-user--login").click(function () { //use a class, since your ID gets mangled
+        $(".tab-user--signup a").removeClass("active"); //add the class to the clicked element
+        $(".tab-user--login a").addClass("active"); //add the class to the clicked element
+    });
+});
+$(function () { //run when the DOM is ready
+    $(".tab-user--signup").click(function () { //use a class, since your ID gets mangled
+        $(".panel-body--tab-content-signup").removeClass("hidden"); //add the class to the clicked element
+        $(".panel-body--tab-content-login").addClass("hidden"); //add the class to the clicked element
+    });
+});
+$(function () { //run when the DOM is ready
+    $(".tab-user--login").click(function () { //use a class, since your ID gets mangled
+        $(".panel-body--tab-content-login").removeClass("hidden"); //add the class to the clicked element
+        $(".panel-body--tab-content-signup").addClass("hidden"); //add the class to the clicked element
+    });
+});
+
 
 // Login floating label
 $(".input-form").each(function () {
@@ -113,13 +169,10 @@ $(".input-form").each(function () {
     });
 });
 
-$(function () { //run when the DOM is ready
-    $(".signup").click(function () { //use a class, since your ID gets mangled
-        $(".panel-login").toggleClass("panel-header--full"); //add the class to the clicked element
-    });
-});
-$(function () { //run when the DOM is ready
-    $(".forgot-password").click(function () { //use a class, since your ID gets mangled
-        $(".panel-login").toggleClass("panel-header--forgot"); //add the class to the clicked element
-    });
+$('.content').scroll(function () {
+    if ($(this).scrollTop() > 0) {
+        $('.header-info--sticky').addClass("sticky");
+    } else {
+        $('.header-info--sticky').removeClass("sticky");
+    }
 });
